d7aef2a31aebef04c42da262f53d63ac9bae6376
Omnisch <fleurdeln@gmail.com>
2024-09-03 22:46:57 +0800

    refine typeset

--- res/create_table_stripped.py
+++ res/create_table_stripped.py
@@ -9,21 +9,20 @@ indices = [
         '字頭',       #0
         '反切',       #1
         '切韻擬音',   #2
-        '字類',       #3
-        '聲形-聲',    #4
-        '聲形-形',    #5
-        '小韻首字',   #6
-        '釋義',       #7
-        '釋義補充',   #8
-        '聲紐',       #9
-        '攝',         #10
-        '韻部',       #11
-        '等',         #12
-        '呼',         #13
-        '聲調',       #14
-        '聲旁',       #15
-        'A/B類分析',  #16
-        '聲形析微',   #17
+        '聲紐',       #3
+        '攝',         #4
+        '韻部',       #5
+        '等',         #6
+        '呼',         #7
+        '聲調',       #8
+        '聲旁',       #9
+        '字類',       #10
+        '形聲-形',    #11
+        '形聲-聲',    #12
+        '釋義',       #13
+        '釋義補充',   #14
+        'A/B類分析',  #15
+        '聲形析微',   #16
     ]
 
 raw = open('guangyun_stripped.csv', 'r', encoding='utf-8')
@@ -35,24 +34,22 @@ with open('kanji_bank_1.json', 'w', encoding='utf-8') as o:
     for line in lines:
         items = line.strip().split(',')
         keyChr = items[0]
-        prn = items[1] + '切　' + items[2]
+        prn = items[1] + '切 ' + items[2]
         
         
         expList = []
-        if items[3] == '':
-            items[3] = '形聲'
-        expList.append('"' + items[3] + '字，' + items[5] + items[4] + '"')
-        if items[6] != '':
-            expList.append('"' + indices[6] + '"')
-        if items[8] != '':
-            expList.append('"' + items[7] + '（' + items[8] + '）"')
+        expList.append('"' + ''.join(items[3:9]) + '　' + items[9] + '聲"')
+        if items[10] == '':
+            items[10] = '形聲'
+        expList.append('"' + items[10] + '字　' + items[11] + items[12] + '"')
+        if items[14] != '':
+            expList.append('"' + items[13] + '（' + items[14] + '）"')
         else:
-            expList.append('"' + items[7] + '"')
-        expList.append('"' + ''.join(items[9:15]) + '　' + items[15] + '聲"')
+            expList.append('"' + items[13] + '"')
+        if items[15] != '':
+            expList.append('"※' + indices[15] + '：' + items[15] + '"')
         if items[16] != '':
-            expList.append('"※' + indices[16] + '：' + items[16] + '"')
-        if items[17] != '':
-            expList.append('"' + indices[17] + '：' + items[17] + '"')
+            expList.append('"' + indices[16] + '：' + items[16] + '"')
         
         o.write('["' + keyChr + '","' + prn + '","","",[' + ','.join(expList) + '],{}],')
     o.write(']')
